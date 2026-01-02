6c7f41e18987a00fd71278dbf58c737dae1d571f
YraBos <60366596+YraBos@users.noreply.github.com>
2026-01-02 20:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const app = express();
 
 app.use(express.json());
 
+// временное хранилище событий в памяти
+const events = [];
+
+// POST — добавление события
 app.post('/api/events', (req, res) => {
   console.log('Получен JSON:', req.body);
+  events.push(req.body); // сохраняем событие в массив
   res.json({ status: 'ok' });
+});
+
+// GET — получение всех событий
+app.get('/api/events', (req, res) => {
+  res.json(events);
+});
+
+// корневой маршрут (чтобы проверить, что сервер жив)
+app.get('/', (req, res) => {
+  res.send('API работает. Используй /api/events');
 });
 
 const PORT = process.env.PORT || 3000;
